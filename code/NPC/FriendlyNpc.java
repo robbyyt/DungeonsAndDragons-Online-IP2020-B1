91eaba52ac9d52747a6857c3082cbb9d07d645d1
liza-i <45426013+liza-i@users.noreply.github.com>
2020-04-07 21:19:15 +0300

    Update FriendlyNpc.java

--- code/NPC/FriendlyNpc.java
+++ code/NPC/FriendlyNpc.java
@@ -1,20 +1,11 @@
-package org.letos;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FriendlyNpc extends Npc{
-    private int id;
-    private List<Integer> drop = new ArrayList<>();
+  
     private List<Integer> items = new ArrayList<>();
-
-    public List<Integer> getDrop() {
-        return drop;
-    }
-
-    public void setDrop(List<Integer> drop) {
-        this.drop = drop;
-    }
 
     public List<Integer> getItems() {
         return items;
@@ -22,13 +13,5 @@ public class FriendlyNpc extends Npc{
 
     public void setItems(List<Integer> items) {
         this.items = items;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 }
