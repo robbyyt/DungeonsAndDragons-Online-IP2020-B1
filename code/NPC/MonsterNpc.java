c54bcac431caaba42d031cea1b1c931933c93cbc
liza-i <45426013+liza-i@users.noreply.github.com>
2020-04-07 21:19:47 +0300

    Update MonsterNpc.java

--- code/NPC/MonsterNpc.java
+++ code/NPC/MonsterNpc.java
@@ -1,25 +1,8 @@
-package org.letos;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MonsterNpc extends Npc{
-    private int id;
-    private List<Integer> drop = new ArrayList<>();
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Integer> getDrop() {
-        return drop;
-    }
-
-    public void setDrop(List<Integer> drop) {
-        this.drop = drop;
-    }
+   
 }
