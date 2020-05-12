b6047fb0893fc71c3d478119cf2d02eb130fe064
Senegeac Catalin Florin <cata.sene12@gmail.com>
2020-05-12 15:56:17 +0300

    lobby almost done

--- UnityProject/Assets/Scripts/Lobby/CurrentPlayer.cs
+++ UnityProject/Assets/Scripts/Lobby/CurrentPlayer.cs
@@ -11,40 +11,53 @@ public class CurrentPlayer : MonoBehaviourPunCallbacks
     public int PlayerLevel;
     public LobbyManager manager;
     public PlayerPanel panel = null;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerRole = Role.UNKNOWN;
         PlayerLevel = 0;
-     
+
     }
 
     // Update is called once per frame
     public override void OnJoinedRoom()
     {
-        if(NickName == "")
+        if (NickName == "")
         {
             Player player = PhotonNetwork.LocalPlayer;
             IsAdmin = player.IsMasterClient;
             NickName = player.NickName;
-            if(NickName != "")
+            if (NickName != "")
             {
-                LobbyPlayer lobbyPlayer = new LobbyPlayer(Guid.Parse(player.UserId), PlayerLevel, NickName, PlayerRole);
-                panel = manager.lobbyPlayersManager.AddPlayerToLobby(lobbyPlayer);
+                if(IsAdmin==true)
+                {
+                    LobbyPlayer lobbyPlayer = new LobbyPlayer(Guid.Parse(player.UserId), PlayerLevel, NickName, Role.ADMIN);
+                    panel = manager.lobbyPlayersManager.AddPlayerToLobby(lobbyPlayer);
+                }
+                else
+                {
+                    LobbyPlayer lobbyPlayer = new LobbyPlayer(Guid.Parse(player.UserId), PlayerLevel, NickName, PlayerRole);
+                    panel = manager.lobbyPlayersManager.AddPlayerToLobby(lobbyPlayer);
+                }    
+                
             }
-          
+
         }
     }
 
     public void UpdateRole(Role role)
     {
-        this.PlayerRole = role;
-        if (this.panel != null)
-        {
-            this.panel.UpdateRole(role);
-        }
-        manager.UpdatePlayerReccomandationRoles();
-        GetComponent<LobbyRpc>().UpdateForGodSake(PhotonNetwork.LocalPlayer.UserId, role);
-    }
+		if(IsAdmin == false){
+        	this.PlayerRole = role;
+			
+        	if (this.panel != null)
+        	{
+            	this.panel.UpdateRole(role);
+        	}
+        	manager.UpdatePlayerReccomandationRoles();
+        	GetComponent<LobbyRpc>().UpdateForGodSake(PhotonNetwork.LocalPlayer.UserId, role);
+    	}
+	}
+
 }
